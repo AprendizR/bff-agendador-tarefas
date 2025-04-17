08c621570835084898b98b161a4c372073516f2d
Renan Rosa <renanrosa1005@gmail.com>
2025-04-17 10:48:12 -0300

    Adicionado client de email

---
+++ src/main/java/com/renan/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,16 @@
+package com.renan.bffagendadortarefas.infrastructure.client;
+
+import com.renan.bffagendadortarefas.business.dto.in.TarefasDTORequest;
+import com.renan.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.renan.bffagendadortarefas.business.enums.StatusNotificacaoEnum;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
